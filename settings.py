ed52733703e8aa8999e0451c84eb073f4f919026
jesicatjan <jesicatjan11@gmail.com>
2024-10-16 21:05:39 +0800

    update ninette ip address

--- settings.py
+++ settings.py
@@ -3,7 +3,8 @@ STM_SERIAL_PORT = '/dev/ttyUSB0'  # Change this if the STM board is connected on
 STM_BAUD_RATE = 115200
 
 # API DETAILS
-ALGO_API_IP = '192.168.31.34'  # IP address of laptop, jesica's server ip address is 192.168.31.34 on RPI wifi
+# ALGO_API_IP = '192.168.31.34'  # IP address of laptop, jesica's server ip address is 192.168.31.34 on RPI wifi
+ALGO_API_IP = '192.168.31.24'  # IP address of laptop, ninette's server ip address is 192.168.31.34 on RPI wifi
 ALGO_API_PORT = 5000
 
 # ROBOT SETTINGS
